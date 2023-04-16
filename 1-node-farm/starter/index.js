ecd2c8c67db4ae516c5fe2f775e45f6394738508
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-15 19:57:37 -0700

    Begin creating API in Node return data.json data

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const path = require("path");
 const url = require("url");
 
 //////////////////////////////////
@@ -39,10 +40,20 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, {
+        "Content-type": "application/json"
+      });
+      res.end(data);
+    });
+
   } else {
     res.writeHead(404, {
-        "Content-type": "text/html",
-        "my-own-header": "hello-world"
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
     });
     res.end("<h1>Page not found!</h1>");
   }
