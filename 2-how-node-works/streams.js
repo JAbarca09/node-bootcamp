1cd70ab7106b5d9261c4fe2a3440b5b1d6e55df6
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-18 19:22:15 -0700

    3 different solutions to read and write data to user

--- 2-how-node-works/streams.js
+++ 2-how-node-works/streams.js
@@ -4,10 +4,33 @@ const server = require("http").createServer();
 server.on("request", (req, res) => {
   // Solution 1, Node will load the entire file in memory, then it will send the data
   // This solution becomes problematic when the file is large, and tons of requests are being made
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
-  });
+  // Never this this code on production!
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+
+  // Solution 2: Streams, suffers from backpressure, the data being read is much faster than the written response
+  // We're streaming the text file, when one piece of the file is read we send it to the client using the
+  // write method of the responsd stream.
+  //   const readable = fs.createReadStream("test-file.txt");
+  //   readable.on("data", (chunk) => {
+  //     // The response is a writable stream
+  //     res.write(chunk);
+  //   });
+  //   readable.on("end", () => {
+  //     res.end();
+  //   });
+  //   readable.on("error", (err) => {
+  //     console.log(err);
+  //     res.statusCode = 500;
+  //     res.end("File not found");
+  //   });
+
+  //Solution 3 use the pipe function
+  const readable = fs.createReadStream("test-file.txt");
+  readable.pipe(res);
+  // readableSource.pipe(writeableDest)
 });
 
 server.listen(8000, "127.0.0.1", () => {
