8f065dc0e22f879ff80fa177e2445f0bfccefa42
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-21 22:21:16 -0700

    Write shorthand for exporting the calulator class

--- 2-how-node-works/test-module-1.js
+++ 2-how-node-works/test-module-1.js
@@ -1,15 +1,27 @@
-class Calculator {
-  add(a, b) {
-    return a + b;
-  }
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
 
-  multiply(a, b) {
-    return a * b;
-  }
+//   multiply(a, b) {
+//     return a * b;
+//   }
 
-  divide(a, b) {
-    return a / b;
-  }
-}
+//   divide(a, b) {
+//     return a / b;
+//   }
+// }
 
-module.exports = Calculator;
+module.exports = class {
+    add(a, b) {
+      return a + b;
+    }
+  
+    multiply(a, b) {
+      return a * b;
+    }
+  
+    divide(a, b) {
+      return a / b;
+    }
+  }
