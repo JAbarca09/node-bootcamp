da5ac7f5ea0eadc20ad5af812063b99abfcaeb1a
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-25 14:35:03 -0700

    Add middleware so we can interact with POST data being sent over

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const express = require('express');
+
 const app = express();
+
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res
@@ -25,6 +28,8 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.post('/api/v1/tours', (req, res) => {});
 
 const port = 3000;
 app.listen(port, () => {
