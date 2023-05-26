8c737daea65ad8e691bf6be3572b657385483eb3
Jesse Abarca <JesseAbarca888@gmail.com>
2023-05-25 23:08:28 -0700

    Get Tour by id

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -29,6 +29,21 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = parseInt(req.params.id, 10);
+  console.log(id);
+  const tour = tours.find(el => el.id === id);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
