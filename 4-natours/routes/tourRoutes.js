d78ba3ae7ef5eba07ed6ca9e45fc6a58ba394030
Jesse Abarca <JesseAbarca888@gmail.com>
2023-06-01 15:54:36 -0700

    Add param middleware for tourRoutes with ids

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -3,11 +3,13 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
+router.param('id', tourController.checkID);
+
 router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
-  
+
 router
   .route('/:id')
   .get(tourController.getTour)
