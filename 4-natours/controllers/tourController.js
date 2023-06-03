5219cb361ef80a4a3a467ab744b18544acc9b41c
Jesse Abarca <JesseAbarca888@gmail.com>
2023-06-02 21:36:58 -0700

    Remove console log from checkBody function

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -16,7 +16,6 @@ exports.checkID = (req, res, next, val) => {
 };
 
 exports.checkBody = (req, res, next) => {
-  console.log('Check tour body:', req.body);
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
