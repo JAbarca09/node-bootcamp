bdb4cdf6477d9fabb3cd131c806b9996edcf4137
Jesse Abarca <JesseAbarca888@gmail.com>
2023-06-05 22:02:59 -0700

    Format app.js and server.js to remove warnings from eslint

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-// console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+console.log(process.env);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
