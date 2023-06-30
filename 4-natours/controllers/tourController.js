20c41789f26e5781b92fa531678ac2c1d5e9a808
Jesse Abarca <JesseAbarca888@gmail.com>
2023-06-29 23:23:31 -0700

    Add paginate method to APIFeatures class

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -52,6 +52,16 @@ class APIFeatures {
 
     return this;
   }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
 }
 
 exports.getAllTours = async (req, res) => {
@@ -90,20 +100,24 @@ exports.getAllTours = async (req, res) => {
     // }
 
     //4) Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
 
-    // page=3&limit=10, 1-10: page 1, 11-20: page 2, 21-30 page 3
-    query = query.skip(skip).limit(limit);
+    // // page=3&limit=10, 1-10: page 1, 11-20: page 2, 21-30 page 3
+    // query = query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist!');
-    }
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error('This page does not exist!');
+    // }
 
     // EXECUTE QUERY
-    const features = APIFeatures(Tour.find(), req.query).filter().sort();
+    const features = APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
     const tours = await features.query;
 
     // SEND RESPONSE
