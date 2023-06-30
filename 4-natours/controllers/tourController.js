33e90d02857a03def3d2905a2e2a69de3b48bdd2
Jesse Abarca <JesseAbarca888@gmail.com>
2023-06-29 23:24:16 -0700

    Update the query in the filter method

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -26,7 +26,7 @@ class APIFeatures {
       /\b(gte|gt|lte|lt|eq)\b/g,
       (match) => `$${match}`
     );
-    this.query.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
 
     return this;
   }
